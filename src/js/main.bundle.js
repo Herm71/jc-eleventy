c324681e04382fc704ebc55fe939dea0da469c89
Jason Chafin <jchafin@ucsc.edu>
2024-10-06 12:21:29 -0700

    Add javsscript files to build pipeline

---
+++ src/js/main.bundle.js
@@ -0,0 +1,26 @@
+(() => {
+  // src/assets/scripts/test.js
+  console.log("loaded properly");
+
+  // src/assets/scripts/theme-switch.js
+  function isLight() {
+    return localStorage.getItem("light-mode");
+  }
+  function toggleRootClass() {
+    document.querySelector(":root").classList.toggle("light");
+  }
+  function toggleLocalStorageItem() {
+    if (isLight()) {
+      localStorage.removeItem("light-mode");
+    } else {
+      localStorage.setItem("light-mode", "set");
+    }
+  }
+  if (isLight()) {
+    toggleRootClass();
+  }
+  document.querySelector(".theme-icon").addEventListener("click", () => {
+    toggleLocalStorageItem();
+    toggleRootClass();
+  });
+})();
