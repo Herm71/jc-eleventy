4a405717f52f41c34100eaedc7cde97be0644b79
Jason Chafin <jchafin@ucsc.edu>
2024-10-11 11:39:09 -0700

    replace original theme switch with new

--- src/js/main.bundle.js
+++ src/js/main.bundle.js
@@ -1,6 +1,8 @@
 (() => {
   // src/assets/scripts/test.js
   console.log("loaded properly");
+
+  // src/assets/scripts/theme-switch.js
   var darkModeState = false;
   var button = document.querySelector(".test-btn");
   var useDark = window.matchMedia("(prefers-color-scheme: dark)");
@@ -16,27 +18,5 @@
     darkModeState = !darkModeState;
     toggleDarkMode(darkModeState);
     setDarkModeLocalStorage(darkModeState);
-  });
-
-  // src/assets/scripts/theme-switch.js
-  function isLight() {
-    return localStorage.getItem("light-mode");
-  }
-  function toggleRootClass() {
-    document.querySelector(":root").classList.toggle("light");
-  }
-  function toggleLocalStorageItem() {
-    if (isLight()) {
-      localStorage.removeItem("light-mode");
-    } else {
-      localStorage.setItem("light-mode", "set");
-    }
-  }
-  if (isLight()) {
-    toggleRootClass();
-  }
-  document.querySelector(".theme-icon").addEventListener("click", () => {
-    toggleLocalStorageItem();
-    toggleRootClass();
   });
 })();
