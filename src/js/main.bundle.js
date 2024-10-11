85c1df4b37c160e02eb1e4446a61085c1c89a8e9
Jason Chafin <jchafin@ucsc.edu>
2024-10-11 11:18:10 -0700

    refactor theme switch working prototype

--- src/js/main.bundle.js
+++ src/js/main.bundle.js
@@ -1,6 +1,22 @@
 (() => {
   // src/assets/scripts/test.js
   console.log("loaded properly");
+  var darkModeState = false;
+  var button = document.querySelector(".test-btn");
+  var useDark = window.matchMedia("(prefers-color-scheme: dark)");
+  function toggleDarkMode(state) {
+    document.documentElement.classList.toggle("dark-mode", state);
+  }
+  function setDarkModeLocalStorage(state) {
+    localStorage.setItem("dark-mode", state);
+  }
+  toggleDarkMode(localStorage.getItem("dark-mode") == "true");
+  useDark.addListener((evt) => toggleDarkMode(evt.matches));
+  button.addEventListener("click", () => {
+    darkModeState = !darkModeState;
+    toggleDarkMode(darkModeState);
+    setDarkModeLocalStorage(darkModeState);
+  });
 
   // src/assets/scripts/theme-switch.js
   function isLight() {
