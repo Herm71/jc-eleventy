9447edf3ccd63ffde5f61805273d4818c8271e32
Jason Chafin <jchafin@ucsc.edu>
2024-10-27 17:28:17 -0700

    Back to top work #35

--- src/js/main.bundle.js
+++ src/js/main.bundle.js
@@ -24,4 +24,28 @@
   document.querySelector(".nav-toggle").addEventListener("click", function() {
     document.querySelector(".nav").classList.toggle("active");
   });
+
+  // src/assets/scripts/back-to-top.js
+  document.addEventListener("DOMContentLoaded", function() {
+    const offset = 100;
+    const speed = 250;
+    const duration = 500;
+    const topButton = document.querySelector(".topbutton");
+    window.addEventListener("scroll", function() {
+      if (window.scrollY < offset) {
+        topButton.style.transition = `opacity ${duration}ms`;
+        topButton.style.opacity = 0;
+      } else {
+        topButton.style.transition = `opacity ${duration}ms`;
+        topButton.style.opacity = 1;
+      }
+    });
+    topButton.addEventListener("click", function(event) {
+      event.preventDefault();
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      });
+    });
+  });
 })();
