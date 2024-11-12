3624f0c70d2dce1d926b393f8a40a352aa3305ee
Jason Chafin <Herm71@users.noreply.github.com>
2024-11-11 17:46:57 -0800

    Add stub. (#49)

---
+++ config/eleventy.config.masonryImage.js
@@ -0,0 +1,40 @@
+const sharp = require('sharp');
+const Image = require('@11ty/eleventy-img');
+
+const GALLERY_IMAGE_WIDTH = 192;
+const LANDSCAPE_LIGHTBOX_IMAGE_WIDTH = 2000;
+const PORTRAIT_LIGHTBOX_IMAGE_WIDTH = 720;
+
+async function galleryImageShortcode(src, alt) {
+    let lightboxImageWidth = LANDSCAPE_LIGHTBOX_IMAGE_WIDTH;
+
+    const metadata = await sharp(src).metadata();
+
+    if(metadata.height > metadata.width) {
+        lightboxImageWidth = PORTRAIT_LIGHTBOX_IMAGE_WIDTH;
+    }
+
+    const options = {
+        formats: ['jpeg'],
+        widths: [GALLERY_IMAGE_WIDTH, lightboxImageWidth],
+        urlPath: "/gen/",
+        outputDir: './_site/gen/'
+    }
+
+    const genMetadata = await Image(src, options);
+
+    return `
+        <a href="${genMetadata.jpeg[1].url}" 
+        data-pswp-width="${genMetadata.jpeg[1].width}" 
+        data-pswp-height="${genMetadata.jpeg[1].height}" 
+        target="_blank">
+            <img src="${genMetadata.jpeg[0].url}" alt="${alt}" />
+        </a>
+    `.replace(/(\r\n|\n|\r)/gm, "");;
+}
+
+module.exports = function(eleventyConfig) {
+    ...
+    eleventyConfig.addLiquidShortcode('galleryImage', galleryImageShortcode)
+    ...
+}
